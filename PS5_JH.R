c7b1062b57a44f59803cb74c8d47753fc192e9bc
jhomola <homola@wustl.edu>
2016-03-04 16:49:35 -0600

    PS5

---
+++ PS5_JH.R
@@ -0,0 +1,28 @@
+## 4625 - R Programming
+## Problem Set 5
+## March 10
+## Name
+
+rm(list=ls())
+set.seed(12435)
+options(stringsAsFactors=F)
+library(foreign)
+
+## read in data
+anes <- read.dta("C:/Users/Jona/Documents/Uni/WashU5/4625-Programming/PS5/anes_timeseries_2012_stata12.dta")
+
+## model Obama's feeling thermometer score as function
+## of Clinton's feeling thermometer score
+model1 <- lm(ft_dpc ~ ft_hclinton, anes)
+
+## make a prediction for a single observation with
+## hypothetical clinton score of 77
+predict(model1, data.frame(ft_hclinton=77))
+## we would expect a Obama score of 71.7
+
+
+## Question 1
+## randomly subset the data into two partitions
+## use "training set" to build at least three models 
+## of Obama's feeling thermometer score
+## document carefully how you deal with missingness
